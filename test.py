203264b5c87bb70876d7ab0f7c2f797f698a0489
alset4 <alekstefanov99@gmail.com>
2024-11-12 16:47:55 -0500

    made a class

---
+++ test.py
@@ -0,0 +1,15 @@
+from cmu_graphics import *
+
+class Test():
+
+    def __init__(self):
+        self.x = 500
+        self.y = 500
+
+
+def main():
+    test = Test()
+    print(test.x, test.y)
+
+main()
+
